0515b3e5067343265a22d530a27eb8e8a6b3fffb
Jitin <jitin.dominic@tothenew.com>
2016-02-23 21:52:05 +0530

    Login test case incomplete

--- test/unit/com/ttnd/linksharing/LoginControllerSpec.groovy
+++ test/unit/com/ttnd/linksharing/LoginControllerSpec.groovy
@@ -17,4 +17,78 @@ class LoginControllerSpec extends Specification {
 
     void "test something"() {
     }
+
+    def "testing login/index when session.user doesn't exist"() {
+        when:
+        controller.index()
+
+        then:
+        session.user == null
+        response.contentAsString == "You must login. ~Failure~"
+    }
+
+    def "testing login/index when session.user exists"() {
+        when:
+        controller.index()
+
+        then:
+        session.user != null
+        response.forwardedUrl == "/user/index"
+    }
+
+    def "testing login/logout"() {
+        when:
+        controller.logout()
+
+        then:
+        session.user == null
+        response.forwardedUrl == "login/index"
+    }
+
+    def "testing login/loginHandler when user is not found"() {
+
+        when:
+        controller.loginHandler(userName, password)
+
+        then:
+        flash.error == "User not found"
+        response.contentAsString == flash.error
+
+        where:
+        userName  | password
+        null      | "wrongPassword"
+        ""        | "wrongPassword"
+        "notInDB" | null
+        "notInDB" | ""
+        "notInDB" | "wrongPassword"
+    }
+
+    def "testing login/loginHandler when user found is inactive"() {
+
+        when:
+        controller.loginHandler(userName, password)
+
+        then:
+        flash.error == "User account is not active"
+        response.contentAsString == flash.error
+
+        where:
+        userName | password
+        "normal" | "defaultPassword"
+    }
+
+    def "testing login/loginHandler when user found is active"() {
+
+        when:
+        controller.loginHandler(userName, password)
+
+        then:
+        session.user != null
+        response.redirectedUrl = "login/index"
+
+        where:
+        userName | password
+        "normal" | "defaultPassword"
+    }
+
 }
