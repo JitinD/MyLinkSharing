03fd68894013e0032ab0bc9e4f8dca37f10197fd
Jitin <jitin.dominic@tothenew.com>
2016-02-29 01:30:33 +0530

    Test cases done

--- test/unit/com/ttnd/linksharing/TopicControllerSpec.groovy
+++ test/unit/com/ttnd/linksharing/TopicControllerSpec.groovy
@@ -1,11 +1,15 @@
 package com.ttnd.linksharing
 
+import enums.Visibility
+import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 
 /**
  * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
  */
+
+@Mock([Topic, User, Subscription])
 @TestFor(TopicController)
 class TopicControllerSpec extends Specification {
 
@@ -25,46 +29,56 @@ class TopicControllerSpec extends Specification {
 
         then:
         flash.error == "User is not subscribed to the topic."
-        response.redirectedUrl == "login/index"
+        response.redirectedUrl == "/"
 
         where:
-        id << [null, 100]
+        id << [null, -1]
     }
 
     def "testing topic/show when topic's visibility is public"() {
 
+        setup:
+        Topic topic = new Topic(id: 1, visibility: Visibility.PUBLIC)
+        topic.save(validate: false)
+
         when:
-        controller.show(id)
+        controller.show(topic.id)
 
         then:
         response.contentAsString == "Topic found and its public. ~SUCCESS~"
-
-        where:
-        id << [1, 2, 3]
     }
 
     def "testing topic/show when topic's visibility is private and user is not subscribed to topic"() {
 
+        Topic topic = new Topic(id: 1, visibility: Visibility.PRIVATE)
+        topic.save(validate: false)
+
+        User user = new User()
+        user.save(validate: false)
+
         when:
-        controller.show(id)
+        controller.show(topic.id)
 
         then:
-        response.contentAsString == "User is subscribed to the topic. ~SUCCESS~"
-
-        where:
-        id << [1, 2, 3]
+        flash.error == "User is not subscribed to the topic."
     }
 
     def "testing topic/show when topic's visibility is private and user is subscribed to topic"() {
 
+        Topic topic = new Topic(id: 1, visibility: Visibility.PRIVATE)
+        topic.save(validate: false)
+
+        User user = new User()
+        user.save(validate: false)
+
+        Subscription subscription = new Subscription(user: user, topic: topic)
+        subscription.save(validate: false)
+
         when:
-        controller.show(id)
+        controller.show(topic.id)
 
         then:
         flash.error == "User is not subscribed to the topic."
-        response.redirectedUrl == "login/index"
-
-        where:
-        id << [1, 2, 3]
+        response.redirectedUrl == "/"
     }
 }
